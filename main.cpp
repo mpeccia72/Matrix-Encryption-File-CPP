4dab1c547858ef94edf4d8979b3c82aa7681f38a
Mike <michaelmpeccia@gmail.com>
2022-11-30 12:46:18 -0600

    update main

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include "./encryption.h"
 
 using namespace std;
 
 int main()
 {
+    int key[2][2] = {
+        {1,2},
+        {3,4}
+    };
+    
+    string fileMessage;
+
     // input and output file objects
     ifstream input;
     ofstream output;
     
     // opens the file of data
     input.open("message.txt");
+    
+    // puts text from input file into a string
+    input >> fileMessage;
     
     // Checks if file fails to input
     if(input.fail()) {
